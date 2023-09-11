26c5634d784c626b90f81b2e76ab45a1c1acde4f
Hugo <hugokorteapple@gmail.com>
2023-09-11 20:40:47 +0200

    added better and more tests

--- src/tests/maskerize.spec.ts
+++ src/tests/maskerize.spec.ts
@@ -8,26 +8,52 @@ const userEvent = userEventDefault as unknown as (typeof userEventDefault)['defa
 const user = userEvent.setup();
 
 describe('Mask', () => {
-	it('Mask formats input to the supplied format', async () => {
+	it('Makserize formats number wildcards', async () => {
 		const mask = '99/99/99';
-		await render(MaskerizeTest, { mask });
+		render(MaskerizeTest, { mask });
+
+		const input = screen.getByTestId('input') as HTMLInputElement;
+		input.focus();
+
+		await user.keyboard('abcdef');
+		await user.keyboard('123456');
+
+		expect(input.value).toBe('12/34/56');
+	});
+	it('Makserize formats letter wildcards', async () => {
+		const mask = 'aa/aa/aa';
+		render(MaskerizeTest, { mask });
 
 		const input = screen.getByTestId('input') as HTMLInputElement;
 		input.focus();
 
 		await user.keyboard('123456');
+		await user.keyboard('abcdef');
 
-		expect(input.value).toBe('12/34/56');
+		expect(input.value).toBe('ab/cd/ef');
 	});
-	it('Mask using numbers does not accept letters', async () => {
-		const mask = '99/99/99';
-		await render(MaskerizeTest, { mask });
+	it('Mask "*" wildcard accepts both numbers and letters', async () => {
+		const mask = '**/**/**';
+		render(MaskerizeTest, { mask });
 
 		const input = screen.getByTestId('input') as HTMLInputElement;
 
 		await user.click(input);
 
-		await user.keyboard('abcdef');
+		await user.keyboard('a1b2c3');
+
+		expect(input.value).toBe('a1/b2/c3');
+	});
+	it('Backspaces do not cause format', async () => {
+		const mask = '99/99/99';
+		render(MaskerizeTest, { mask });
+
+		const input = screen.getByTestId('input') as HTMLInputElement;
+
+		await user.click(input);
+
+		await user.keyboard('999999');
+		await user.keyboard('{backspace>8/}');
 
 		expect(input.value).toBe('');
 	});
